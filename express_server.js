b6bcc91277c2772aa51eb87bc2ac517f96ed53ba
andy zhen <andyzhen44@gmail.com>
2022-03-21 18:29:07 -0700

    new urls_new form page created to have users enter URL. POST request sent to server and processed.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
 app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
+const res = require('express/lib/response');
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,6 +12,10 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send('hello!')
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
@@ -22,25 +28,22 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-})
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World<b></body></html>\n')
-})
-
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-})
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-})
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT}`)
 });
+
+function generateRandomString() {
+  let result = '';
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 6; i ++) {
+    randomChar = char.charAt(Math.floor(Math.random() * char.length))
+    result += randomChar;
+  }
+  return result;
+}
+
